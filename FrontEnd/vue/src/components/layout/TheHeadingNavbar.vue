8e763b12f12e27e4758c2356004c7a425f2056ae
hanjaehyeon <hjaehyeon98@gmail.com>
2023-11-21 17:59:42 +0900

    feat: 메인 페이지 이미지 변경 및 리다이렉트 기능 구현

--- FrontEnd/vue/src/components/layout/TheHeadingNavbar.vue
+++ FrontEnd/vue/src/components/layout/TheHeadingNavbar.vue
@@ -24,8 +24,17 @@ const logout = () => {
     <div class="container-fluid">
       <router-link :to="{ name: 'main' }" class="navbar-brand">
         <a class="navbar-brand">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-airplane" viewBox="0 0 16 16">
-            <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Zm.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1c-.213 0-.458.158-.678.599Z"/>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-airplane"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Zm.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1c-.213 0-.458.158-.678.599Z"
+            />
           </svg>
           EnjoyTrip
         </a>
@@ -47,9 +56,6 @@ const logout = () => {
           style="--bs-scroll-height: 100px"
         >
           <li class="nav-item">
-            <a class="nav-link" href="#">마이캠퍼스</a>
-          </li>
-          <li class="nav-item">
             <router-link :to="{ name: 'playground' }" class="nav-link">놀이터</router-link>
           </li>
           <li class="nav-item dropdown">
@@ -66,24 +72,14 @@ const logout = () => {
               <li><a class="dropdown-item" href="#">공지사항</a></li>
               <li><a class="dropdown-item" href="#">FAQ</a></li>
               <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">학사규정</a></li>
             </ul>
           </li>
           <li class="nav-item">
             <router-link :to="{ name: 'board' }" class="nav-link">게시판</router-link>
           </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'estations' }" class="nav-link">전기차충전소</router-link>
-          </li>
-          <!-- 이현호 테스트 Vue Draggable -->
-          <li class="nav-item">
-            <router-link :to="{ name: 'draggable' }" class="nav-link"
-              >Vue 3 Draggable 테스트</router-link
-            >
-          </li>
           <!-- 이현호 테스트 여행지 정보 -->
           <li class="nav-item">
-            <router-link :to="{ name: 'map' }" class="nav-link">여행지 지도 테스트</router-link>
+            <router-link :to="{ name: 'map' }" class="nav-link">여행 계획 짜기</router-link>
           </li>
         </ul>
         <form class="d-flex" role="search">
